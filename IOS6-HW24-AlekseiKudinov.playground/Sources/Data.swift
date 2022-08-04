9beead49947a8d59ef55859194f24d3070cb9b9d
AlekseiKudinov <machette94@gmail.com>
2022-08-04 08:24:02 +0300

    Bug fix.

---
+++ IOS6-HW24-AlekseiKudinov.playground/Sources/Data.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public class DataFetcher {
+
+    public static func fetchCards(urlRequest: URL?, completion: @escaping (Cards?) -> Void) {
+        Network.getData(urlRequest: urlRequest) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let cards = try JSONDecoder().decode(Cards.self, from: data)
+                    completion(cards)
+                } catch let jsonError {
+                    print("Failed to decode JSON: \(jsonError)")
+                }
+            case .failure:
+                completion(nil)
+            }
+        }
+    }
+}
+
--- IOS6-HW24-AlekseiKudinov.playground/Sources/Model.swift
+++ IOS6-HW24-AlekseiKudinov.playground/Sources/Model.swift
@@ -5,10 +5,35 @@ public struct Cards: Decodable {
 }
 
 public struct Card: Decodable {
-    public let name: String
-    public let manaCost: String?
-    public let type: String?
-    public let rarity: String?
-    public let setName: String?
+    let name: String
+    let manaCost: String?
+    let type: String
+    let rarity: String
+    let setName: String
+    let artist: String
+    let power: String?
+
+    public static func cardsData(_ card: Card) {
+        guard card.name == "Opt" || card.name == "Black Lotus" else { return }
+
+        print("""
+        Card name: \(card.name)
+        Type: \(card.type)
+        Rarity: \(card.rarity)
+        Set name: \(card.setName)
+        Author: \(card.artist)
+        """)
+
+        if let manaCost = card.manaCost {
+            print("Mana cost: \(manaCost)")
+        }
+
+        if let power = card.power {
+            print("Power: \(power)")
+        }
+
+        print("--------------------------------")
+    }
 }
+
 
